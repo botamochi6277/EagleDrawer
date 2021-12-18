5e33255cec10a2b7e3ed086ac5d73992a0d4be5b
botamochi6277 <botamochi6277@icloud.com>
2021-12-18 23:08:11 +0900

    fix width

--- src/viewer.py
+++ src/viewer.py
@@ -2,9 +2,19 @@ import xml.etree.ElementTree as ET
 import argparse
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+
+from enum import Enum
+
+
+class Unit(Enum):
+    MM = 0
+    INCH = 1
+    MIL = 2
+
+
 plt.rcParams["svg.fonttype"] = "none"
-plt.rcParams["figure.facecolor"] = 'black'
-plt.rcParams["axes.facecolor"] = 'black'
+plt.rcParams["figure.facecolor"] = '#2b2b2b'
+plt.rcParams["axes.facecolor"] = '#2b2b2b'
 
 # https://coolors.co/0696d7-0dab76-32c8c8-c90d15-ffba08
 # 17
@@ -12,6 +22,11 @@ eagle_colors = ['#ffffff', '#0696D7', '#0DAB76', '32C8C8',
                 '#C90D15', '#FFBA08', '#C8C832', '#808080',
                 '#282828', '#8252C2', '#00ff00', '#00ffff',
                 '#ff0000', '#ff00ff', '#FFCD07', '#AFD134']
+
+
+def inch_to_point(inch):
+    dpi = 72  # dot per inch
+    return inch * dpi
 
 
 def search_layer(layers: ET.ElementTree, no: int):
@@ -29,11 +44,12 @@ def draw_circle(circle: ET.ElementTree, layers: ET.ElementTree, ax: plt.Axes):
     y = float(attr['y'])
     r = float(attr['radius'])
     w = float(attr['width'])
+    # inch to point
     layer_no = int(attr['layer'])
     layer = search_layer(layers, layer_no)
     color_id = int(layer.attrib['color'])
     c = patches.Circle(xy=(x, y), radius=r, ec=eagle_colors[color_id],
-                       fill=False, linewidth=w)
+                       fill=False, linewidth=inch_to_point(w))
     ax.add_patch(c)
 
 
