4b44f27f2091ec73acca7065e5dd6feb23c8fc47
botamochi6277 <botamochi6277@icloud.com>
2022-02-06 16:31:24 +0900

    add node parser

--- src/svg_parser.py
+++ src/svg_parser.py
@@ -1,4 +1,6 @@
 import argparse
+from ast import parse
+from typing import Tuple
 
 import xml.etree.ElementTree as ET
 import numpy as np
@@ -23,6 +25,28 @@ def parse_tf(s):
     return mat
 
 
+def parse_node(s: str) -> Tuple:
+    """parse nodes in a path
+
+    Args:
+        s (str): body of the path's d attribute
+
+    Returns:
+        Tuple: (symbol, positions)
+    """
+    # https://developer.mozilla.org/ja/docs/Web/SVG/Tutorial/Paths
+    sy = re.findall(r'[a-zA-Z]', s)
+    l = re.split('[a-zA-Z]', s)  # sprit with alphabet
+    pos = [p.split(',') for p in l[1:]]
+
+    # convert str to float
+    for p in pos:
+        for i, x in enumerate(p):
+            p[i] = float(x)
+
+    return sy, pos
+
+
 def seek_tree(tree: ET.ElementTree, tf=[]):
     for child in tree:
         tf_c = copy.deepcopy(tf)
@@ -39,9 +63,12 @@ def seek_tree(tree: ET.ElementTree, tf=[]):
 
         if tag == '{http://www.w3.org/2000/svg}g':
             seek_tree(child, tf_c)
+        elif tag == '{http://www.w3.org/2000/svg}path':
+            d = attr['d']
+            print(d)
         else:
-            # print(f'{tag}')
-            print(f'tf: {tf_c}')
+            print(f'{tag}')
+            # print(f'tf: {tf_c}')
 
 
 def show_text(filename):
@@ -49,11 +76,14 @@ def show_text(filename):
     root = tree.getroot()
 
     print(f'{root.tag}')
-    print('---')
     seek_tree(root, [])
 
+    print('---')
     print(parse_tf('matrix(1,0,0,1,210,30)'))
     print(parse_tf("matrix(0.0889855,0,0,-0.0889855,-388.103,185.482)"))
+
+    print(parse_node('M7483,1883L7496,1883L7533,1993'))
+    print(parse_node('M7456.92,1809.5L7534.08,1809.5'))
 
 
 if __name__ == '__main__':
